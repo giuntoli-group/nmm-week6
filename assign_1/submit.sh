b36c3078ad2a268f342e4856dfd7207abd8aebca
nayan_habrok <n.vengallur@rug.nl>
2025-05-21 14:21:06 +0200

    updated assign_1 files

--- assign_1/submit.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH --job-name=PLA_all_atom
-#SBATCH --time=08:00:00
-#SBATCH --partition=parallelshort
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=2
-
-module load LAMMPS/23Jun2022-foss-2021b-kokkos
-srun lmp -in PLA_CHARMM.in > out.lammps
